baedeb498c0b4aedbffc02ff75bf74899e5ed0dd
Sheyla Souza <slls.sheyla@gmail.com>
2021-11-09 00:17:20 -0300

    Aprendendo Sequelize

--- sequelize-demo/db.js
+++ sequelize-demo/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('crud', 'root', 'luiztools', {
+const sequelize = new Sequelize('crud', 'root', 'sheylatools', {
 
     dialect: 'mysql',
     host: 'localhost',
